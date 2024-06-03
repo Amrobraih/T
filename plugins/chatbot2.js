5bb33ac967a97af56448acebc9e21c33e52e26dc
Amrobraih <154635591+Amrobraih@users.noreply.github.com>
2024-06-03 18:39:23 +0300

    Update chatbot2.js

--- plugins/chatbot2.js
+++ plugins/chatbot2.js
@@ -3,11 +3,11 @@ handler.all = async function (m) {
 let chat = global.db.data.chats[m.chat] 
 
 if (/^مين عمك|مين عمك؟|من عمك/i.test(m.text) ) {
-     conn.reply(m.chat, `*ريــمــورو ســـامـــا💜✨*\n*𝚂𝚑𝚊𝚛𝚔𝚢᯽𝚂𝚊𝚖𝚊あ*\n*wa.me/966540235070*`, m)
+     conn.reply(m.chat, `*عـمـرو ســـامـــا💜✨*\n*𝑮𝒐𝒌𝒖᯽𝚂𝚊𝚖𝚊あ*\n*wa.me/967774318278*`, m)
 }
 
-if (/^مين سيدتك|مين سيدتك|مين عمتك؟|مين عمتك|سيدتك من/i.test(m.text) ) {
-     conn.reply(m.chat, `*مــيــلــيــم ســـامـــا💜✨*\n\n`, m)
+if (/^مين سيدك|مين سيدك؟|مين العم؟|مين العم|سيدك من/i.test(m.text) ) {
+     conn.reply(m.chat, `*عــمــرو ســـامـــا💜✨*\n\n`, m)
 }
 return !0 } 
 
